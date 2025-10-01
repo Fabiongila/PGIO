7ec190d8fd1bc741c9308b5b24d36595203861ed
Sejogal <sethlussueki@gmail.com>
2025-10-01 21:46:54 +0100

    ...

--- accounts/static/script/script.js
+++ accounts/static/script/script.js
@@ -1,8 +1,17 @@
+const hour = () =>{
+            const date = new Date();
+            let hora = date.getHours()
+            let minuto = date.getMinutes()
+            let segundo = date.getSeconds()
+            document.getElementById("hour").innerHTML=`${hora}:${minuto}:${segundo}`
+        }
+        setInterval(hour,1000)
+
 //IMAGENS
 var i = 0;
 
 let imagens = [
-    '/static/img-index/2.jpg',
+    '/static/ui/1.png',
     '/static/img-index/4.jpg',
     '/static/img-index/2.jpg',
     '/static/img-index/10.jpg'
@@ -42,5 +51,3 @@ setInterval(change,7000)
 function botaodep(){
     document.getElementById("hidden").classList.remove("hidden")
 }
-
-// document.body.style.backgroundImage=imagens[i]
