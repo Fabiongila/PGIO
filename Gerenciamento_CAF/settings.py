d9ee822d15714c8f130f9ac8b861863ace7f7f65
Fabiongila <fabiongila@mail.com>
2025-10-03 16:53:26 +0100

    configurações de hospedagem.2

--- Gerenciamento_CAF/settings.py
+++ Gerenciamento_CAF/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +48,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Gerenciamento_CAF.urls'
+
+ALLOWED_HOSTS = ['seu-app.onrender.com']
+
+
+
 
 #Para fazer a leitura de arquivos estaticos
 STATIC_URL = '/static'
@@ -81,8 +87,14 @@ WSGI_APPLICATION = 'Gerenciamento_CAF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #Estou configurando para hospedagem
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+
     }
 }
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Grenciamento_CAF.wsgi:application
